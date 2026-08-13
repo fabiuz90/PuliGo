133fbc872942e96e12c6118685a20cbade901a9c
Fabio <fabiograf90@gmail.com>
2026-08-13 17:14:28 +0200

    Remove remaining Base44 fallback fragments

--- ForgotPassword.jsx
+++ ForgotPassword.jsx
@@ -1,4 +1,3 @@
- || { auth:{ isAuthenticated: async()=>false, me: async()=>null }, entities:new Proxy({}, { get:()=>({ filter:async()=>[], get:async()=>null, create:async()=>({}), update:async()=>({}), delete:async()=>({}) }) }), integrations:{ Core:{ UploadFile:async()=>({ file_url:'' }) } } };
 
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
